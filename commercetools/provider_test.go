a783b401546d90f7dc22bf9d96d15773dde258a8
Tim Leguijt <info@leguijtict.nl>
2018-08-23 11:22:44 +0200

    WIP: add AWS resources to acc test case

--- commercetools/provider_test.go
+++ commercetools/provider_test.go
@@ -1,8 +1,11 @@
 package commercetools
 
 import (
+	"fmt"
 	"os"
 	"testing"
+
+	// "github.com/terraform-providers/terraform-provider-aws/aws"
 
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/hashicorp/terraform/terraform"
@@ -11,10 +14,15 @@ import (
 var testAccProviders map[string]terraform.ResourceProvider
 var testAccProviderFactories func(providers *[]*schema.Provider) map[string]terraform.ResourceProviderFactory
 var testAccProvider *schema.Provider
+var testAccAWSProvider *schema.Provider
 
 func init() {
 	testAccProvider = Provider().(*schema.Provider)
+	// TODO: The following raises an type assertion error
+	// Fix the AWS provider in order for the acceptance tests to work
+	// testAccAWSProvider = aws.Provider().(*schema.Provider)
 	testAccProviders = map[string]terraform.ResourceProvider{
+		// "aws":           testAccAWSProvider,
 		"commercetools": testAccProvider,
 	}
 	testAccProviderFactories = func(providers *[]*schema.Provider) map[string]terraform.ResourceProviderFactory {
@@ -54,4 +62,30 @@ func testAccPreCheck(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+}
+
+// Creates a SQS configuration
+func testAccSQSConfig(suffix string) string {
+	return fmt.Sprintf(`
+resource "aws_iam_user" "ct" {
+	name = "commercetools-acctest-%[1]s"
+}
+	
+resource "aws_iam_user_policy_attachment" "sqs" {
+	user       = "${aws_iam_user.ct.name}"
+	policy_arn = "arn:aws:iam::aws:policy/AmazonSQSFullAccess"
+}
+	
+resource "aws_iam_access_key" "ct" {
+	user = "${aws_iam_user.ct.name}"
+}
+	
+resource "aws_sqs_queue" "ct_queue" {
+	name                      = "commercetools-acctest-queue-%[1]s"
+	delay_seconds             = 90
+	max_message_size          = 2048
+	message_retention_seconds = 86400
+	receive_wait_time_seconds = 10
+}
+`, suffix)
 }
--- commercetools/resource_subscription_test.go
+++ commercetools/resource_subscription_test.go
@@ -22,28 +22,39 @@ func TestAccSubscription_basic(t *testing.T) {
 				Config:      testAccSubscriptionConfig(rName, false),
 				ExpectError: regexp.MustCompile("A test message could not be delivered to this destination: SQS.*"),
 			},
-			// {
-			// 	Config: testAccSubscriptionConfig(rName, true),
-			// 	Check: resource.ComposeTestCheckFunc(
-			// 		testAccSubscriptionPresence,
-			// 	),
-			// },
+			{
+				Config: testAccSubscriptionConfig(rName, true),
+				Check: resource.ComposeTestCheckFunc(
+					testAccSubscriptionPresence,
+				),
+			},
 		},
 	})
 }
 
 func testAccSubscriptionConfig(rName string, withAWSResources bool) string {
-	// TODO: Create AWS resources to test with when withAWSResources is true
+	var config string
 
-	return fmt.Sprintf(`
+	queueURL := "https://sqs.eu-west-1.amazonaws.com/0000000000/some-queue"
+	accessKey := "some-access-key"
+	secretKey := "some-secret-key"
+
+	if withAWSResources {
+		config += testAccSQSConfig(rName)
+		queueURL = "${aws_sqs_queue.ct_queue.id}"
+		accessKey = "${aws_iam_access_key.ct.id}"
+		secretKey = "${aws_iam_access_key.ct.secret}"
+	}
+
+	config += fmt.Sprintf(`
 resource "commercetools_subscription" "subscription_%[1]s" {
 	key = "commercetools-acc-%[1]s"
 	
 	destination {
 		type          = "SQS"
-		queue_url     = "https://sqs.eu-west-1.amazonaws.com/0000000000/terraform-queue-%[1]s"
-		access_key    = "accesskey_%[1]s"
-		access_secret = "secret_%[1]s"
+		queue_url     = "%[2]s"
+		access_key    = "%[3]s"
+		access_secret = "%[4]s"
 		region        = "eu-west-1"
 	}
 	
@@ -57,13 +68,16 @@ resource "commercetools_subscription" "subscription_%[1]s" {
 		types = ["ProductPublished", "ProductCreated"]
 	}
 }
-`, rName)
+`, rName, queueURL, accessKey, secretKey)
+	return config
 }
 
-// func testAccSubscriptionPresence(s *terraform.State) error {
-// 	return nil
-// }
+func testAccSubscriptionPresence(s *terraform.State) error {
+	// TODO: Implement
+	return nil
+}
 
 func testAccCheckSubscriptionDestroy(s *terraform.State) error {
+	// TODO: Implement
 	return nil
 }
