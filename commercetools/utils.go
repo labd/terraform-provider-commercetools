1c262baef8bf4f52d4c83cb5991ffbacda7fae3c
Tim Leguijt <info@leguijtict.nl>
2018-09-11 11:36:02 +0200

    Implemented update actions and prevent field types from being altered once created

--- commercetools/utils.go
+++ commercetools/utils.go
@@ -3,6 +3,7 @@ package commercetools
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 
 	"github.com/labd/commercetools-go-sdk/commercetools"
 )
@@ -33,8 +34,34 @@ func localizedStringToMap(input commercetools.LocalizedString) map[string]interf
 
 func stringFormatObject(object interface{}) string {
 	data, err := json.MarshalIndent(object, "", "    ")
+
 	if err != nil {
 		return fmt.Sprintf("%+v", object)
 	}
 	return string(append(data, '\n'))
+}
+
+func stringFormatErrorExtras(err commercetools.Error) string {
+	switch len(err.Errors) {
+	case 0:
+		return ""
+	case 1:
+		return stringFormatObject(err.Errors[0].Extra())
+	default:
+		{
+			messages := make([]string, len(err.Errors))
+			for i, item := range err.Errors {
+				messages[i] = fmt.Sprintf(" %d. %s", i+1, stringFormatObject(item.Extra()))
+			}
+			return strings.Join(messages, "\n")
+		}
+	}
+}
+
+func stringFormatActions(actions commercetools.UpdateActions) string {
+	lines := make([]string, len(actions))
+	for i, action := range actions {
+		lines[i] = fmt.Sprintf("%d: %s", i, stringFormatObject(action))
+	}
+	return strings.Join(lines, "\n")
 }
