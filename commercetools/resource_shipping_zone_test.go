86eb42646bdee9a3fa0685f05dd4fc8f5b9bd0e6
Wouter Westendorp <wouter@gtnetworks.nl>
2019-03-07 16:15:04 +0100

    Additional unit test for shipping zone

--- commercetools/resource_shipping_zone_test.go
+++ commercetools/resource_shipping_zone_test.go
@@ -68,6 +68,144 @@ resource "commercetools_shipping_zone" "standard" {
 }`, name, description)
 }
 
+func TestAccShippingZone_createAndAddLocation(t *testing.T) {
+
+	name := "name"
+	description := "description"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckShippingZoneDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccShippingZoneConfig(name, description),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"commercetools_shipping_zone.standard", "name", name,
+					),
+					resource.TestCheckResourceAttr(
+						"commercetools_shipping_zone.standard", "location.0.country", "DE",
+					),
+					resource.TestCheckResourceAttr(
+						"commercetools_shipping_zone.standard", "location.1.country", "US",
+					),
+					resource.TestCheckResourceAttr(
+						"commercetools_shipping_zone.standard", "location.1.state", "Nevada",
+					),
+				),
+			},
+			{
+				Config: testAccShippingZoneConfigLocationAdded(),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"commercetools_shipping_zone.standard", "name", "the zone",
+					),
+					resource.TestCheckResourceAttr(
+						"commercetools_shipping_zone.standard", "description", "the description",
+					),
+					resource.TestCheckResourceAttr(
+						"commercetools_shipping_zone.standard", "location.#", "3",
+					),
+					resource.TestCheckResourceAttr(
+						"commercetools_shipping_zone.standard", "location.0.country", "DE",
+					),
+					resource.TestCheckResourceAttr(
+						"commercetools_shipping_zone.standard", "location.1.country", "ES",
+					),
+					resource.TestCheckResourceAttr(
+						"commercetools_shipping_zone.standard", "location.2.country", "US",
+					),
+					resource.TestCheckResourceAttr(
+						"commercetools_shipping_zone.standard", "location.2.state", "Nevada",
+					),
+				),
+			},
+		},
+	})
+}
+
+func testAccShippingZoneConfigLocationAdded() string {
+	return `
+resource "commercetools_shipping_zone" "standard" {
+	name = "the zone"
+	description = "the description"
+	location = {
+		country = "DE"
+	}
+	location = {
+		country = "ES"
+	}
+	location = {
+		country = "US"
+		state = "Nevada"
+	}
+}`
+}
+
+func TestAccShippingZone_createAndRemoveLocation(t *testing.T) {
+
+	name := "name"
+	description := "description"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckShippingZoneDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccShippingZoneConfig(name, description),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"commercetools_shipping_zone.standard", "name", name,
+					),
+					resource.TestCheckResourceAttr(
+						"commercetools_shipping_zone.standard", "location.0.country", "DE",
+					),
+					resource.TestCheckResourceAttr(
+						"commercetools_shipping_zone.standard", "location.1.country", "US",
+					),
+					resource.TestCheckResourceAttr(
+						"commercetools_shipping_zone.standard", "location.1.state", "Nevada",
+					),
+				),
+			},
+			{
+				Config: testAccShippingZoneConfigLocationAdded(),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"commercetools_shipping_zone.standard", "name", "the zone",
+					),
+					resource.TestCheckResourceAttr(
+						"commercetools_shipping_zone.standard", "description", "the description",
+					),
+					resource.TestCheckResourceAttr(
+						"commercetools_shipping_zone.standard", "location.#", "1",
+					),
+					resource.TestCheckResourceAttr(
+						"commercetools_shipping_zone.standard", "location.0.country", "US",
+					),
+					resource.TestCheckResourceAttr(
+						"commercetools_shipping_zone.standard", "location.0.state", "Nevada",
+					),
+				),
+			},
+		},
+	})
+}
+
+func testAccShippingZoneConfigLocationRemoved() string {
+	return `
+resource "commercetools_shipping_zone" "standard" {
+	name = "the zone"
+	description = "the description"
+	location = {
+		country = "US"
+		state = "Nevada"
+	}
+}`
+}
+
 func testAccCheckShippingZoneDestroy(s *terraform.State) error {
 	return nil
 }
