1da3702765546b6a2056dbb4c6ae4a26d08540c1
David Weterings <david@davidweterings.nl>
2019-03-14 13:35:12 +0100

    Remove rates from tax category resource

--- commercetools/resource_tax_category_test.go
+++ commercetools/resource_tax_category_test.go
@@ -1,26 +1,53 @@
 package commercetools
 
 import (
+	"fmt"
 	"testing"
 
-	"github.com/hashicorp/terraform/helper/acctest"
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
 )
 
-func TestAccTaxCategoryCreate_basic(t *testing.T) {
-	rName := acctest.RandString(5)
+func TestAccTaxCategory_createAndUpdate(t *testing.T) {
+
+	name := "test category"
+	key := "test-category"
+	description := "test category description"
+
+	newName := "new test category"
+	newKey := "new-test-category"
+	newDescription := "new test category description"
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
-		CheckDestroy: testAccCheckProjectDestroy,
+		CheckDestroy: testAccCheckTaxCategoryDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccTaxCategoryConfig(rName),
+				Config: testAccTaxCategoryConfig(name, key, description),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(
-						"commercetools_tax_category.standard", "name", "Testing tax category",
+						"commercetools_tax_category.standard", "name", name,
+					),
+					resource.TestCheckResourceAttr(
+						"commercetools_tax_category.standard", "key", key,
+					),
+					resource.TestCheckResourceAttr(
+						"commercetools_tax_category.standard", "description", description,
+					),
+				),
+			},
+			{
+				Config: testAccTaxCategoryConfig(newName, newKey, newDescription),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"commercetools_tax_category.standard", "name", newName,
+					),
+					resource.TestCheckResourceAttr(
+						"commercetools_tax_category.standard", "key", newKey,
+					),
+					resource.TestCheckResourceAttr(
+						"commercetools_tax_category.standard", "description", newDescription,
 					),
 				),
 			},
@@ -28,36 +55,15 @@ func TestAccTaxCategoryCreate_basic(t *testing.T) {
 	})
 }
 
-func testAccCheckTaxCategoryDestroy(s *terraform.State) error {
-	return nil
+func testAccTaxCategoryConfig(name string, key string, description string) string {
+	return fmt.Sprintf(`
+resource "commercetools_tax_category" "standard" {
+	name = "%s"
+	key = "%s"
+	description = "%s"
+}`, name, key, description)
 }
 
-func testAccTaxCategoryConfig(name string) string {
-	return `resource "commercetools_tax_category" "standard" {
-	name = "Testing tax category"
-	rate {
-		name = "19% MwSt"
-		amount = 0.19
-		included_in_price = false
-		country = "DE"
-	}
-	rate {
-		name = "21% BTW"
-		amount = 0.21
-		country = "NL"
-		included_in_price = false
-	}
-	rate {
-		name = "5% US"
-		amount = 0.05
-		country = "US"
-		included_in_price = true
-	}
-	rate {
-		name = "0% VAT"
-		amount = 0.0
-		included_in_price = true
-		country = "GB"
-	}
-	}`
+func testAccCheckTaxCategoryDestroy(s *terraform.State) error {
+	return nil
 }
