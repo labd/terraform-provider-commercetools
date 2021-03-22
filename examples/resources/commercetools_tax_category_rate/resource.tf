444c6b58346cf2650069a4e625a533daefac756b
Tim Leguijt <info@leguijtict.nl>
2021-03-22 11:38:43 +0100

    Add examples

---
+++ examples/resources/commercetools_tax_category_rate/resource.tf
@@ -0,0 +1,20 @@
+resource "commercetools_tax_category" "standard" {
+  name = "Standard tax category"
+  key  = "standard-tax-category"
+}
+
+resource "commercetools_tax_category_rate" "standard-tax-category-DE" {
+  tax_category_id   = "${commercetools_tax_category.standard.id}"
+  name              = "19% MwSt"
+  amount            = 0.19
+  included_in_price = false
+  country           = "DE"
+}
+
+resource "commercetools_tax_category_rate" "standard-tax-category-NL" {
+  tax_category_id   = "${commercetools_tax_category.standard.id}"
+  name              = "21% BTW"
+  amount            = 0.21
+  included_in_price = true
+  country           = "NL"
+}
---
+++ examples/resources/commercetools_type/resource.tf
@@ -0,0 +1,79 @@
+resource "commercetools_type" "my-custom-type" {
+  key = "contact_info"
+  name = {
+    en = "Contact info"
+    nl = "Contact informatie"
+  }
+  description = {
+    en = "All things related communication"
+    nl = "Alle communicatie-gerelateerde zaken"
+  }
+
+  resource_type_ids = ["customer"]
+
+  field {
+    name = "skype_name"
+    label = {
+      en = "Skype name"
+      nl = "Skype naam"
+    }
+    type {
+      name = "String"
+    }
+
+  field {
+    name = "contact_time"
+    label = {
+      en = "Contact time"
+      nl = "Contact tijd"
+    }
+    type {
+      name = "Enum"
+      values = {
+        day = "Daytime"
+        evening = "Evening"
+      }
+    }
+  }
+
+  field {
+    name = "emails"
+
+    label = {
+      en = "Emails"
+      nl = "Emails"
+    }
+
+    type = {
+      name = "Set"
+      element_type {
+        name = "String"
+      }
+    }
+  }
+
+  field {
+    name = "contact_preference"
+    label = {
+      en = "Contact preference"
+      nl = "Contact voorkeur"
+    }
+    type {
+      name = "LocalizedEnum"
+      localized_value {
+        key = "phone"
+        label = {
+          en = "Phone"
+          nl = "Telefoon"
+        }
+      }
+      localized_value {
+        key = "skype"
+        label = {
+          en = "Skype"
+          nl = "Skype"
+        }
+      }
+    }
+  }
+}
