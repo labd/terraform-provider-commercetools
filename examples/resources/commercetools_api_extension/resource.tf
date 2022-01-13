e911f6263ee42f05a29bcf40f7b60715cb4a8386
Tim Leguijt <info@leguijtict.nl>
2022-01-13 14:17:51 +0100

    Regenerate docs

--- examples/resources/commercetools_api_extension/resource.tf
+++ examples/resources/commercetools_api_extension/resource.tf
@@ -1,7 +1,7 @@
 resource "commercetools_api_extension" "my-extension" {
   key = "test-case"
 
-  destination {
+  destination = {
     type                 = "HTTP"
     url                  = "https://example.com"
     authorization_header = "Basic 12345"
