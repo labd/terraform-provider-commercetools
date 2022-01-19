bb6a7d0969f3e2139d5198fde0dad84789de1929
Michael van Tellingen <m.vantellingen@labdigital.nl>
2022-01-19 08:54:01 +0100

    Rewrite API Client resource for new plugin framework

--- commercetools/resource_api_client.go
+++ commercetools/resource_api_client.go
@@ -1,118 +1,118 @@
 package commercetools
 
-import (
-	"context"
-	"sort"
-	"strings"
-	"time"
+// import (
+// 	"context"
+// 	"sort"
+// 	"strings"
+// 	"time"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/labd/commercetools-go-sdk/platform"
-)
+// 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+// 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+// 	"github.com/labd/commercetools-go-sdk/platform"
+// )
 
-func resourceAPIClient() *schema.Resource {
-	return &schema.Resource{
-		Description: "Create a new API client. Note that Commercetools might return slightly different scopes, " +
-			"resulting in a new API client being created everytime Terraform is run. In this case, " +
-			"fix your scopes accordingly to match what is returned by Commercetools.\n\n" +
-			"Also see the [API client HTTP API documentation](https://docs.commercetools.com//http-api-projects-api-clients).",
-		Create: resourceAPIClientCreate,
-		Read:   resourceAPIClientRead,
-		Delete: resourceAPIClientDelete,
-		Importer: &schema.ResourceImporter{
-			State: schema.ImportStatePassthrough,
-		},
-		Schema: map[string]*schema.Schema{
-			"name": {
-				Description: "Name of the API client",
-				Type:        schema.TypeString,
-				Required:    true,
-				ForceNew:    true,
-			},
-			"scope": {
-				Description: "A list of the [OAuth scopes](https://docs.commercetools.com/http-api-authorization.html#scopes)",
-				Type:        schema.TypeSet,
-				Elem:        &schema.Schema{Type: schema.TypeString},
-				Required:    true,
-				ForceNew:    true,
-			},
-			"secret": {
-				Type:      schema.TypeString,
-				Computed:  true,
-				Sensitive: true,
-			},
-		},
-	}
-}
+// func resourceAPIClient() *schema.Resource {
+// 	return &schema.Resource{
+// 		Description: "Create a new API client. Note that Commercetools might return slightly different scopes, " +
+// 			"resulting in a new API client being created everytime Terraform is run. In this case, " +
+// 			"fix your scopes accordingly to match what is returned by Commercetools.\n\n" +
+// 			"Also see the [API client HTTP API documentation](https://docs.commercetools.com//http-api-projects-api-clients).",
+// 		Create: resourceAPIClientCreate,
+// 		Read:   resourceAPIClientRead,
+// 		Delete: resourceAPIClientDelete,
+// 		Importer: &schema.ResourceImporter{
+// 			State: schema.ImportStatePassthrough,
+// 		},
+// 		Schema: map[string]*schema.Schema{
+// 			"name": {
+// 				Description: "Name of the API client",
+// 				Type:        types.StringType,
+// 				Required:    true,
+// 				ForceNew:    true,
+// 			},
+// 			"scope": {
+// 				Description: "A list of the [OAuth scopes](https://docs.commercetools.com/http-api-authorization.html#scopes)",
+// 				Type:        schema.TypeSet,
+// 				Elem:        &schema.Schema{Type: types.StringType},
+// 				Required:    true,
+// 				ForceNew:    true,
+// 			},
+// 			"secret": {
+// 				Type:      types.StringType,
+// 				Computed:  true,
+// 				Sensitive: true,
+// 			},
+// 		},
+// 	}
+// }
 
-func resourceAPIClientCreate(d *schema.ResourceData, m interface{}) error {
-	name := d.Get("name").(string)
-	scopes := d.Get("scope").(*schema.Set).List()
+// func resourceAPIClientCreate(d *schema.ResourceData, m interface{}) error {
+// 	name := d.Get("name").(string)
+// 	scopes := d.Get("scope").(*schema.Set).List()
 
-	scopeParts := make([]string, 0)
-	for i := 0; i < len(scopes); i++ {
-		scopeParts = append(scopeParts, scopes[i].(string))
-	}
+// 	scopeParts := make([]string, 0)
+// 	for i := 0; i < len(scopes); i++ {
+// 		scopeParts = append(scopeParts, scopes[i].(string))
+// 	}
 
-	draft := platform.ApiClientDraft{
-		Name:  name,
-		Scope: strings.Join(scopeParts, " "),
-	}
+// 	draft := platform.ApiClientDraft{
+// 		Name:  name,
+// 		Scope: strings.Join(scopeParts, " "),
+// 	}
 
-	client := getClient(m)
+// 	client := getClient(m)
 
-	var apiClient *platform.ApiClient
+// 	var apiClient *platform.ApiClient
 
-	err := resource.Retry(20*time.Second, func() *resource.RetryError {
-		var err error
+// 	err := resource.Retry(20*time.Second, func() *resource.RetryError {
+// 		var err error
 
-		apiClient, err = client.ApiClients().Post(draft).Execute(context.Background())
-		if err != nil {
-			return handleCommercetoolsError(err)
-		}
-		return nil
-	})
+// 		apiClient, err = client.ApiClients().Post(draft).Execute(context.Background())
+// 		if err != nil {
+// 			return handleCommercetoolsError(err)
+// 		}
+// 		return nil
+// 	})
 
-	if err != nil {
-		return err
-	}
+// 	if err != nil {
+// 		return err
+// 	}
 
-	d.SetId(apiClient.ID)
-	d.Set("secret", apiClient.Secret)
+// 	d.SetId(apiClient.ID)
+// 	d.Set("secret", apiClient.Secret)
 
-	return resourceAPIClientRead(d, m)
-}
+// 	return resourceAPIClientRead(d, m)
+// }
 
-func resourceAPIClientRead(d *schema.ResourceData, m interface{}) error {
-	client := getClient(m)
-	apiClient, err := client.ApiClients().WithId(d.Id()).Get().Execute(context.Background())
+// func resourceAPIClientRead(d *schema.ResourceData, m interface{}) error {
+// 	client := getClient(m)
+// 	apiClient, err := client.ApiClients().WithId(d.Id()).Get().Execute(context.Background())
 
-	if err != nil {
-		if ctErr, ok := err.(platform.ErrorResponse); ok {
-			if ctErr.StatusCode == 404 {
-				d.SetId("")
-				return nil
-			}
-		}
-		return err
-	}
+// 	if err != nil {
+// 		if ctErr, ok := err.(platform.ErrorResponse); ok {
+// 			if ctErr.StatusCode == 404 {
+// 				d.SetId("")
+// 				return nil
+// 			}
+// 		}
+// 		return err
+// 	}
 
-	d.SetId(apiClient.ID)
-	d.Set("name", apiClient.Name)
-	scopes := strings.Split(apiClient.Scope, " ")
-	sort.Strings(scopes)
-	d.Set("scope", scopes)
-	return nil
-}
+// 	d.SetId(apiClient.ID)
+// 	d.Set("name", apiClient.Name)
+// 	scopes := strings.Split(apiClient.Scope, " ")
+// 	sort.Strings(scopes)
+// 	d.Set("scope", scopes)
+// 	return nil
+// }
 
-func resourceAPIClientDelete(d *schema.ResourceData, m interface{}) error {
-	client := getClient(m)
+// func resourceAPIClientDelete(d *schema.ResourceData, m interface{}) error {
+// 	client := getClient(m)
 
-	_, err := client.ApiClients().WithId(d.Id()).Delete().Execute(context.Background())
-	if err != nil {
-		return err
-	}
+// 	_, err := client.ApiClients().WithId(d.Id()).Delete().Execute(context.Background())
+// 	if err != nil {
+// 		return err
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
