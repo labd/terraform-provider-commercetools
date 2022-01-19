2f32fe02ba2daa0f566cd6c054357be57bb3d0a8
Michael van Tellingen <m.vantellingen@labdigital.nl>
2022-01-19 08:55:14 +0100

    Rewrite Shipping Method resource for new plugin framework

--- commercetools/resource_shipping_method.go
+++ commercetools/resource_shipping_method.go
@@ -1,248 +1,248 @@
 package commercetools
 
-import (
-	"context"
-	"log"
-	"time"
+// import (
+// 	"context"
+// 	"log"
+// 	"time"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/labd/commercetools-go-sdk/platform"
-)
+// 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+// 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+// 	"github.com/labd/commercetools-go-sdk/platform"
+// )
 
-func resourceShippingMethod() *schema.Resource {
-	return &schema.Resource{
-		Description: "With Shipping Methods you can specify which shipping services you want to provide to your " +
-			"customers for deliveries to different areas of the world at rates you can define.\n\n" +
-			"See also the [Shipping Methods API Documentation](https://docs.commercetoolstools.com/api/projects/shippingMethods)",
-		Create: resourceShippingMethodCreate,
-		Read:   resourceShippingMethodRead,
-		Update: resourceShippingMethodUpdate,
-		Delete: resourceShippingMethodDelete,
-		Importer: &schema.ResourceImporter{
-			State: schema.ImportStatePassthrough,
-		},
-		Schema: map[string]*schema.Schema{
-			"key": {
-				Description: "User-specific unique identifier for the shipping method",
-				Type:        schema.TypeString,
-				Optional:    true,
-			},
-			"name": {
-				Type:     schema.TypeString,
-				Required: true,
-			},
-			"description": {
-				Type:     schema.TypeString,
-				Optional: true,
-			},
-			"localized_description": {
-				Description: "[LocalizedString](https://docs.commercetoolstools.com/api/types#localizedstring)",
-				Type:        TypeLocalizedString,
-				Optional:    true,
-			},
-			"is_default": {
-				Description: "One shipping method in a project can be default",
-				Type:        schema.TypeBool,
-				Optional:    true,
-			},
-			"version": {
-				Type:     schema.TypeInt,
-				Computed: true,
-			},
-			"tax_category_id": {
-				Description: "ID of a [Tax Category](https://docs.commercetoolstools.com/api/projects/taxCategories#taxcategory)",
-				Type:        schema.TypeString,
-				Optional:    true,
-			},
-			"predicate": {
-				Description: "A Cart predicate which can be used to more precisely select a shipping method for a cart",
-				Type:        schema.TypeString,
-				Optional:    true,
-			},
-		},
-	}
-}
+// func resourceShippingMethod() *schema.Resource {
+// 	return &schema.Resource{
+// 		Description: "With Shipping Methods you can specify which shipping services you want to provide to your " +
+// 			"customers for deliveries to different areas of the world at rates you can define.\n\n" +
+// 			"See also the [Shipping Methods API Documentation](https://docs.commercetoolstools.com/api/projects/shippingMethods)",
+// 		Create: resourceShippingMethodCreate,
+// 		Read:   resourceShippingMethodRead,
+// 		Update: resourceShippingMethodUpdate,
+// 		Delete: resourceShippingMethodDelete,
+// 		Importer: &schema.ResourceImporter{
+// 			State: schema.ImportStatePassthrough,
+// 		},
+// 		Schema: map[string]*schema.Schema{
+// 			"key": {
+// 				Description: "User-specific unique identifier for the shipping method",
+// 				Type:        types.StringType,
+// 				Optional:    true,
+// 			},
+// 			"name": {
+// 				Type:     types.StringType,
+// 				Required: true,
+// 			},
+// 			"description": {
+// 				Type:     types.StringType,
+// 				Optional: true,
+// 			},
+// 			"localized_description": {
+// 				Description: "[LocalizedString](https://docs.commercetoolstools.com/api/types#localizedstring)",
+// 				Type:        TypeLocalizedString,
+// 				Optional:    true,
+// 			},
+// 			"is_default": {
+// 				Description: "One shipping method in a project can be default",
+// 				Type:        types.BoolType,
+// 				Optional:    true,
+// 			},
+// 			"version": {
+// 				Type:     types.Int64Type,
+// 				Computed: true,
+// 			},
+// 			"tax_category_id": {
+// 				Description: "ID of a [Tax Category](https://docs.commercetoolstools.com/api/projects/taxCategories#taxcategory)",
+// 				Type:        types.StringType,
+// 				Optional:    true,
+// 			},
+// 			"predicate": {
+// 				Description: "A Cart predicate which can be used to more precisely select a shipping method for a cart",
+// 				Type:        types.StringType,
+// 				Optional:    true,
+// 			},
+// 		},
+// 	}
+// }
 
-func resourceShippingMethodCreate(d *schema.ResourceData, m interface{}) error {
-	client := getClient(m)
-	var shippingMethod *platform.ShippingMethod
-	taxCategory := platform.TaxCategoryResourceIdentifier{}
-	if taxCategoryID, ok := d.GetOk("tax_category_id"); ok {
-		taxCategory.ID = stringRef(taxCategoryID)
-	}
+// func resourceShippingMethodCreate(d *schema.ResourceData, m interface{}) error {
+// 	client := getClient(m)
+// 	var shippingMethod *platform.ShippingMethod
+// 	taxCategory := platform.TaxCategoryResourceIdentifier{}
+// 	if taxCategoryID, ok := d.GetOk("tax_category_id"); ok {
+// 		taxCategory.ID = stringRef(taxCategoryID)
+// 	}
 
-	localizedDescription := platform.LocalizedString(
-		expandStringMap(d.Get("localized_description").(map[string]interface{})))
+// 	localizedDescription := platform.LocalizedString(
+// 		expandStringMap(d.Get("localized_description").(map[string]interface{})))
 
-	draft := platform.ShippingMethodDraft{
-		Key:                  stringRef(d.Get("key")),
-		Name:                 d.Get("name").(string),
-		Description:          stringRef(d.Get("description")),
-		LocalizedDescription: &localizedDescription,
-		IsDefault:            d.Get("is_default").(bool),
-		TaxCategory:          taxCategory,
-		Predicate:            stringRef(d.Get("predicate")),
-	}
+// 	draft := platform.ShippingMethodDraft{
+// 		Key:                  stringRef(d.Get("key")),
+// 		Name:                 d.Get("name").(string),
+// 		Description:          stringRef(d.Get("description")),
+// 		LocalizedDescription: &localizedDescription,
+// 		IsDefault:            d.Get("is_default").(bool),
+// 		TaxCategory:          taxCategory,
+// 		Predicate:            stringRef(d.Get("predicate")),
+// 	}
 
-	err := resource.Retry(1*time.Minute, func() *resource.RetryError {
-		var err error
+// 	err := resource.Retry(1*time.Minute, func() *resource.RetryError {
+// 		var err error
 
-		shippingMethod, err = client.ShippingMethods().Post(draft).Execute(context.Background())
-		if err != nil {
-			return handleCommercetoolsError(err)
-		}
-		return nil
-	})
+// 		shippingMethod, err = client.ShippingMethods().Post(draft).Execute(context.Background())
+// 		if err != nil {
+// 			return handleCommercetoolsError(err)
+// 		}
+// 		return nil
+// 	})
 
-	if err != nil {
-		return err
-	}
+// 	if err != nil {
+// 		return err
+// 	}
 
-	if shippingMethod == nil {
-		log.Fatal("No shipping method created?")
-	}
+// 	if shippingMethod == nil {
+// 		log.Fatal("No shipping method created?")
+// 	}
 
-	d.SetId(shippingMethod.ID)
-	d.Set("version", shippingMethod.Version)
+// 	d.SetId(shippingMethod.ID)
+// 	d.Set("version", shippingMethod.Version)
 
-	return resourceShippingMethodRead(d, m)
-}
+// 	return resourceShippingMethodRead(d, m)
+// }
 
-func resourceShippingMethodRead(d *schema.ResourceData, m interface{}) error {
-	log.Printf("[DEBUG] Reading shipping method from commercetools, with shippingMethod id: %s", d.Id())
+// func resourceShippingMethodRead(d *schema.ResourceData, m interface{}) error {
+// 	log.Printf("[DEBUG] Reading shipping method from commercetools, with shippingMethod id: %s", d.Id())
 
-	client := getClient(m)
+// 	client := getClient(m)
 
-	shippingMethod, err := client.ShippingMethods().WithId(d.Id()).Get().Execute(context.Background())
+// 	shippingMethod, err := client.ShippingMethods().WithId(d.Id()).Get().Execute(context.Background())
 
-	if err != nil {
-		if ctErr, ok := err.(platform.ErrorResponse); ok {
-			if ctErr.StatusCode == 404 {
-				d.SetId("")
-				return nil
-			}
-		}
-		return err
-	}
+// 	if err != nil {
+// 		if ctErr, ok := err.(platform.ErrorResponse); ok {
+// 			if ctErr.StatusCode == 404 {
+// 				d.SetId("")
+// 				return nil
+// 			}
+// 		}
+// 		return err
+// 	}
 
-	if shippingMethod == nil {
-		log.Print("[DEBUG] No shipping method found")
-		d.SetId("")
-	} else {
-		log.Print("[DEBUG] Found following shipping method:")
-		log.Print(stringFormatObject(shippingMethod))
+// 	if shippingMethod == nil {
+// 		log.Print("[DEBUG] No shipping method found")
+// 		d.SetId("")
+// 	} else {
+// 		log.Print("[DEBUG] Found following shipping method:")
+// 		log.Print(stringFormatObject(shippingMethod))
 
-		d.Set("version", shippingMethod.Version)
-		d.Set("key", shippingMethod.Key)
-		d.Set("name", shippingMethod.Name)
-		d.Set("description", shippingMethod.Description)
-		d.Set("localized_description", shippingMethod.LocalizedDescription)
-		d.Set("is_default", shippingMethod.IsDefault)
-		d.Set("tax_category_id", shippingMethod.TaxCategory.ID)
-		d.Set("predicate", shippingMethod.Predicate)
-	}
+// 		d.Set("version", shippingMethod.Version)
+// 		d.Set("key", shippingMethod.Key)
+// 		d.Set("name", shippingMethod.Name)
+// 		d.Set("description", shippingMethod.Description)
+// 		d.Set("localized_description", shippingMethod.LocalizedDescription)
+// 		d.Set("is_default", shippingMethod.IsDefault)
+// 		d.Set("tax_category_id", shippingMethod.TaxCategory.ID)
+// 		d.Set("predicate", shippingMethod.Predicate)
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
 
-func resourceShippingMethodUpdate(d *schema.ResourceData, m interface{}) error {
-	ctMutexKV.Lock(d.Id())
-	defer ctMutexKV.Unlock(d.Id())
+// func resourceShippingMethodUpdate(d *schema.ResourceData, m interface{}) error {
+// 	ctMutexKV.Lock(d.Id())
+// 	defer ctMutexKV.Unlock(d.Id())
 
-	client := getClient(m)
-	shippingMethod, err := client.ShippingMethods().WithId(d.Id()).Get().Execute(context.Background())
-	if err != nil {
-		return err
-	}
+// 	client := getClient(m)
+// 	shippingMethod, err := client.ShippingMethods().WithId(d.Id()).Get().Execute(context.Background())
+// 	if err != nil {
+// 		return err
+// 	}
 
-	input := platform.ShippingMethodUpdate{
-		Version: shippingMethod.Version,
-		Actions: []platform.ShippingMethodUpdateAction{},
-	}
+// 	input := platform.ShippingMethodUpdate{
+// 		Version: shippingMethod.Version,
+// 		Actions: []platform.ShippingMethodUpdateAction{},
+// 	}
 
-	if d.HasChange("name") {
-		newName := d.Get("name").(string)
-		input.Actions = append(
-			input.Actions,
-			&platform.ShippingMethodChangeNameAction{Name: newName})
-	}
+// 	if d.HasChange("name") {
+// 		newName := d.Get("name").(string)
+// 		input.Actions = append(
+// 			input.Actions,
+// 			&platform.ShippingMethodChangeNameAction{Name: newName})
+// 	}
 
-	if d.HasChange("key") {
-		newKey := d.Get("key").(string)
-		input.Actions = append(
-			input.Actions,
-			&platform.ShippingMethodSetKeyAction{Key: &newKey})
-	}
+// 	if d.HasChange("key") {
+// 		newKey := d.Get("key").(string)
+// 		input.Actions = append(
+// 			input.Actions,
+// 			&platform.ShippingMethodSetKeyAction{Key: &newKey})
+// 	}
 
-	if d.HasChange("description") {
-		newDescription := d.Get("description").(string)
-		input.Actions = append(
-			input.Actions,
-			&platform.ShippingMethodSetDescriptionAction{Description: &newDescription})
-	}
+// 	if d.HasChange("description") {
+// 		newDescription := d.Get("description").(string)
+// 		input.Actions = append(
+// 			input.Actions,
+// 			&platform.ShippingMethodSetDescriptionAction{Description: &newDescription})
+// 	}
 
-	if d.HasChange("localized_description") {
-		newLocalizedDescription := platform.LocalizedString(
-			expandStringMap(d.Get("localized_description").(map[string]interface{})))
-		input.Actions = append(
-			input.Actions,
-			&platform.ShippingMethodSetLocalizedDescriptionAction{LocalizedDescription: &newLocalizedDescription})
-	}
+// 	if d.HasChange("localized_description") {
+// 		newLocalizedDescription := platform.LocalizedString(
+// 			expandStringMap(d.Get("localized_description").(map[string]interface{})))
+// 		input.Actions = append(
+// 			input.Actions,
+// 			&platform.ShippingMethodSetLocalizedDescriptionAction{LocalizedDescription: &newLocalizedDescription})
+// 	}
 
-	if d.HasChange("is_default") {
-		newIsDefault := d.Get("is_default").(bool)
-		input.Actions = append(
-			input.Actions,
-			&platform.ShippingMethodChangeIsDefaultAction{IsDefault: newIsDefault})
-	}
+// 	if d.HasChange("is_default") {
+// 		newIsDefault := d.Get("is_default").(bool)
+// 		input.Actions = append(
+// 			input.Actions,
+// 			&platform.ShippingMethodChangeIsDefaultAction{IsDefault: newIsDefault})
+// 	}
 
-	if d.HasChange("tax_category_id") {
-		taxCategoryID := d.Get("tax_category_id").(string)
-		input.Actions = append(
-			input.Actions,
-			&platform.ShippingMethodChangeTaxCategoryAction{TaxCategory: platform.TaxCategoryResourceIdentifier{ID: &taxCategoryID}})
-	}
+// 	if d.HasChange("tax_category_id") {
+// 		taxCategoryID := d.Get("tax_category_id").(string)
+// 		input.Actions = append(
+// 			input.Actions,
+// 			&platform.ShippingMethodChangeTaxCategoryAction{TaxCategory: platform.TaxCategoryResourceIdentifier{ID: &taxCategoryID}})
+// 	}
 
-	if d.HasChange("predicate") {
-		newPredicate := d.Get("predicate").(string)
-		input.Actions = append(
-			input.Actions,
-			&platform.ShippingMethodSetPredicateAction{Predicate: &newPredicate})
-	}
+// 	if d.HasChange("predicate") {
+// 		newPredicate := d.Get("predicate").(string)
+// 		input.Actions = append(
+// 			input.Actions,
+// 			&platform.ShippingMethodSetPredicateAction{Predicate: &newPredicate})
+// 	}
 
-	log.Printf(
-		"[DEBUG] Will perform update operation with the following actions:\n%s",
-		stringFormatActions(input.Actions))
+// 	log.Printf(
+// 		"[DEBUG] Will perform update operation with the following actions:\n%s",
+// 		stringFormatActions(input.Actions))
 
-	_, err = client.ShippingMethods().WithId(shippingMethod.ID).Post(input).Execute(context.Background())
-	if err != nil {
-		if ctErr, ok := err.(platform.ErrorResponse); ok {
-			log.Printf("[DEBUG] %v: %v", ctErr, stringFormatErrorExtras(ctErr))
-		}
-		return err
-	}
+// 	_, err = client.ShippingMethods().WithId(shippingMethod.ID).Post(input).Execute(context.Background())
+// 	if err != nil {
+// 		if ctErr, ok := err.(platform.ErrorResponse); ok {
+// 			log.Printf("[DEBUG] %v: %v", ctErr, stringFormatErrorExtras(ctErr))
+// 		}
+// 		return err
+// 	}
 
-	return resourceShippingMethodRead(d, m)
-}
+// 	return resourceShippingMethodRead(d, m)
+// }
 
-func resourceShippingMethodDelete(d *schema.ResourceData, m interface{}) error {
-	client := getClient(m)
+// func resourceShippingMethodDelete(d *schema.ResourceData, m interface{}) error {
+// 	client := getClient(m)
 
-	ctMutexKV.Lock(d.Id())
-	defer ctMutexKV.Unlock(d.Id())
+// 	ctMutexKV.Lock(d.Id())
+// 	defer ctMutexKV.Unlock(d.Id())
 
-	shippingMethod, err := client.ShippingMethods().WithId(d.Id()).Get().Execute(context.Background())
-	if err != nil {
-		return err
-	}
+// 	shippingMethod, err := client.ShippingMethods().WithId(d.Id()).Get().Execute(context.Background())
+// 	if err != nil {
+// 		return err
+// 	}
 
-	_, err = client.ShippingMethods().WithId(d.Id()).Delete().WithQueryParams(platform.ByProjectKeyShippingMethodsByIDRequestMethodDeleteInput{
-		Version: shippingMethod.Version,
-	}).Execute(context.Background())
-	if err != nil {
-		return err
-	}
+// 	_, err = client.ShippingMethods().WithId(d.Id()).Delete().WithQueryParams(platform.ByProjectKeyShippingMethodsByIDRequestMethodDeleteInput{
+// 		Version: shippingMethod.Version,
+// 	}).Execute(context.Background())
+// 	if err != nil {
+// 		return err
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
--- commercetools/resource_shipping_method_test.go
+++ commercetools/resource_shipping_method_test.go
@@ -1,145 +1,145 @@
 package commercetools
 
-import (
-	"context"
-	"fmt"
-	"testing"
+// import (
+// 	"context"
+// 	"fmt"
+// 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-)
+// 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+// 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+// )
 
-func TestAccShippingMethod_createAndUpdateWithID(t *testing.T) {
+// func TestAccShippingMethod_createAndUpdateWithID(t *testing.T) {
 
-	name := "test sh method"
-	key := "test-sh-method"
-	description := "test shipping method description"
-	predicate := "1 = 1"
+// 	name := "test sh method"
+// 	key := "test-sh-method"
+// 	description := "test shipping method description"
+// 	predicate := "1 = 1"
 
-	newName := "new test sh method"
-	newKey := "new-test-sh-method"
-	newDescription := "new test shipping method description"
-	newPredicate := "2 = 2"
+// 	newName := "new test sh method"
+// 	newKey := "new-test-sh-method"
+// 	newDescription := "new test shipping method description"
+// 	newPredicate := "2 = 2"
 
-	resource.Test(t, resource.TestCase{
-		PreCheck:     func() { testAccPreCheck(t) },
-		Providers:    testAccProviders,
-		CheckDestroy: testAccCheckShippingMethodDestroy,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccShippingMethodConfig(name, key, description, description, false, true, predicate),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(
-						"commercetools_shipping_method.standard", "name", name,
-					),
-					resource.TestCheckResourceAttr(
-						"commercetools_shipping_method.standard", "key", key,
-					),
-					resource.TestCheckResourceAttr(
-						"commercetools_shipping_method.standard", "description", description,
-					),
-					resource.TestCheckResourceAttr(
-						"commercetools_shipping_method.standard", "localized_description.en", description,
-					),
-					resource.TestCheckResourceAttr(
-						"commercetools_shipping_method.standard", "is_default", "false",
-					),
-					resource.TestCheckResourceAttr(
-						"commercetools_shipping_method.standard", "predicate", predicate,
-					),
-				),
-			},
-			{
-				Config: testAccShippingMethodConfig(newName, newKey, newDescription, newDescription, true, true, newPredicate),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(
-						"commercetools_shipping_method.standard", "name", newName,
-					),
-					resource.TestCheckResourceAttr(
-						"commercetools_shipping_method.standard", "key", newKey,
-					),
-					resource.TestCheckResourceAttr(
-						"commercetools_shipping_method.standard", "description", newDescription,
-					),
-					resource.TestCheckResourceAttr(
-						"commercetools_shipping_method.standard", "localized_description.en", newDescription,
-					),
-					resource.TestCheckResourceAttr(
-						"commercetools_shipping_method.standard", "is_default", "true",
-					),
-					resource.TestCheckResourceAttrSet(
-						"commercetools_shipping_method.standard", "tax_category_id",
-					),
-					resource.TestCheckResourceAttr(
-						"commercetools_shipping_method.standard", "predicate", newPredicate,
-					),
-				),
-			},
-		},
-	})
-}
+// 	resource.Test(t, resource.TestCase{
+// 		PreCheck:     func() { testAccPreCheck(t) },
+// 		Providers:    testAccProviders,
+// 		CheckDestroy: testAccCheckShippingMethodDestroy,
+// 		Steps: []resource.TestStep{
+// 			{
+// 				Config: testAccShippingMethodConfig(name, key, description, description, false, true, predicate),
+// 				Check: resource.ComposeTestCheckFunc(
+// 					resource.TestCheckResourceAttr(
+// 						"commercetools_shipping_method.standard", "name", name,
+// 					),
+// 					resource.TestCheckResourceAttr(
+// 						"commercetools_shipping_method.standard", "key", key,
+// 					),
+// 					resource.TestCheckResourceAttr(
+// 						"commercetools_shipping_method.standard", "description", description,
+// 					),
+// 					resource.TestCheckResourceAttr(
+// 						"commercetools_shipping_method.standard", "localized_description.en", description,
+// 					),
+// 					resource.TestCheckResourceAttr(
+// 						"commercetools_shipping_method.standard", "is_default", "false",
+// 					),
+// 					resource.TestCheckResourceAttr(
+// 						"commercetools_shipping_method.standard", "predicate", predicate,
+// 					),
+// 				),
+// 			},
+// 			{
+// 				Config: testAccShippingMethodConfig(newName, newKey, newDescription, newDescription, true, true, newPredicate),
+// 				Check: resource.ComposeTestCheckFunc(
+// 					resource.TestCheckResourceAttr(
+// 						"commercetools_shipping_method.standard", "name", newName,
+// 					),
+// 					resource.TestCheckResourceAttr(
+// 						"commercetools_shipping_method.standard", "key", newKey,
+// 					),
+// 					resource.TestCheckResourceAttr(
+// 						"commercetools_shipping_method.standard", "description", newDescription,
+// 					),
+// 					resource.TestCheckResourceAttr(
+// 						"commercetools_shipping_method.standard", "localized_description.en", newDescription,
+// 					),
+// 					resource.TestCheckResourceAttr(
+// 						"commercetools_shipping_method.standard", "is_default", "true",
+// 					),
+// 					resource.TestCheckResourceAttrSet(
+// 						"commercetools_shipping_method.standard", "tax_category_id",
+// 					),
+// 					resource.TestCheckResourceAttr(
+// 						"commercetools_shipping_method.standard", "predicate", newPredicate,
+// 					),
+// 				),
+// 			},
+// 		},
+// 	})
+// }
 
-func testAccShippingMethodConfig(name string, key string, description string, localizedDescription string, isDefault bool, setTaxCategory bool, predicate string) string {
-	taxCategoryReference := ""
-	if setTaxCategory {
-		taxCategoryReference = "tax_category_id = \"${commercetools_tax_category.test.id}\""
-	}
-	return fmt.Sprintf(`
-resource "commercetools_tax_category" "test" {
-	name = "test"
-	key = "test"
-	description = "test"
-}
+// func testAccShippingMethodConfig(name string, key string, description string, localizedDescription string, isDefault bool, setTaxCategory bool, predicate string) string {
+// 	taxCategoryReference := ""
+// 	if setTaxCategory {
+// 		taxCategoryReference = "tax_category_id = \"${commercetools_tax_category.test.id}\""
+// 	}
+// 	return fmt.Sprintf(`
+// resource "commercetools_tax_category" "test" {
+// 	name = "test"
+// 	key = "test"
+// 	description = "test"
+// }
 
-resource "commercetools_shipping_method" "standard" {
-	name = "%s"
-	key = "%s"
-	description = "%s"
-	localized_description = {
-		en = "%s"
-	}
-	is_default = "%t"
-	predicate = "%s"
+// resource "commercetools_shipping_method" "standard" {
+// 	name = "%s"
+// 	key = "%s"
+// 	description = "%s"
+// 	localized_description = {
+// 		en = "%s"
+// 	}
+// 	is_default = "%t"
+// 	predicate = "%s"
 
-	%s
-	`, name, key, description, localizedDescription, isDefault, predicate, taxCategoryReference) + "\n}\n"
-}
+// 	%s
+// 	`, name, key, description, localizedDescription, isDefault, predicate, taxCategoryReference) + "\n}\n"
+// }
 
-func testAccCheckShippingMethodDestroy(s *terraform.State) error {
-	client := getClient(testAccProvider.Meta())
+// func testAccCheckShippingMethodDestroy(s *terraform.State) error {
+// 	client := getClient(testAccProvider.Meta())
 
-	for _, rs := range s.RootModule().Resources {
-		switch rs.Type {
-		case "commercetools_tax_category":
-			{
-				response, err := client.TaxCategories().WithId(rs.Primary.ID).Get().Execute(context.Background())
-				if err == nil {
-					if response != nil && response.ID == rs.Primary.ID {
-						return fmt.Errorf("tax category (%s) still exists", rs.Primary.ID)
-					}
-					continue
-				}
+// 	for _, rs := range s.RootModule().Resources {
+// 		switch rs.Type {
+// 		case "commercetools_tax_category":
+// 			{
+// 				response, err := client.TaxCategories().WithId(rs.Primary.ID).Get().Execute(context.Background())
+// 				if err == nil {
+// 					if response != nil && response.ID == rs.Primary.ID {
+// 						return fmt.Errorf("tax category (%s) still exists", rs.Primary.ID)
+// 					}
+// 					continue
+// 				}
 
-				if newErr := checkApiResult(err); newErr != nil {
-					return newErr
-				}
-			}
-		case "commercetools_shipping_method":
-			{
-				response, err := client.ShippingMethods().WithId(rs.Primary.ID).Get().Execute(context.Background())
-				if err == nil {
-					if response != nil && response.ID == rs.Primary.ID {
-						return fmt.Errorf("shipping method (%s) still exists", rs.Primary.ID)
-					}
-					continue
-				}
-				if newErr := checkApiResult(err); newErr != nil {
-					return newErr
-				}
-			}
-		default:
-			continue
-		}
-	}
-	return nil
-}
+// 				if newErr := checkApiResult(err); newErr != nil {
+// 					return newErr
+// 				}
+// 			}
+// 		case "commercetools_shipping_method":
+// 			{
+// 				response, err := client.ShippingMethods().WithId(rs.Primary.ID).Get().Execute(context.Background())
+// 				if err == nil {
+// 					if response != nil && response.ID == rs.Primary.ID {
+// 						return fmt.Errorf("shipping method (%s) still exists", rs.Primary.ID)
+// 					}
+// 					continue
+// 				}
+// 				if newErr := checkApiResult(err); newErr != nil {
+// 					return newErr
+// 				}
+// 			}
+// 		default:
+// 			continue
+// 		}
+// 	}
+// 	return nil
+// }
