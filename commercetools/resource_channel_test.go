0f33ebc4e94955d68536639102b8f1b371be6b7c
Alexander Miehe <alexander.miehe@flaconi.de>
2022-02-16 12:09:47 +0100

    * use mock server now

--- commercetools/resource_channel_test.go
+++ commercetools/resource_channel_test.go
@@ -3,7 +3,6 @@ package commercetools
 import (
 	"context"
 	"fmt"
-	"os"
 	"testing"
 
 	"github.com/labd/commercetools-go-sdk/platform"
@@ -12,15 +11,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
-func skipMockServer(t *testing.T) {
-	if os.Getenv("CTP_CLIENT_ID") == "unittest" {
-		t.Skip("Skipping testing with mock server as the implementation can not handle custom fields with key reference instead of id reference")
-	}
-}
-
 func TestAccChannelCreate_basic(t *testing.T) {
-
-	skipMockServer(t)
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
@@ -84,8 +75,6 @@ func TestAccChannelCreate_basic(t *testing.T) {
 }
 
 func TestAccChannelCreate_updateCustom(t *testing.T) {
-
-	skipMockServer(t)
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
