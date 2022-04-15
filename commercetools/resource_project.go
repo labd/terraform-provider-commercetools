1b7799b04d22200213dfe7f0a34ca21c89e1a9e5
Onur Gunduz <o.gunduz@labdigital.nl>
2022-04-15 14:17:05 +0200

    Compute shipping_rate_input_type when not set

--- commercetools/resource_project.go
+++ commercetools/resource_project.go
@@ -147,6 +147,7 @@ func resourceProjectSettings() *schema.Resource {
 					"shippingRateInput field on the cart to select a tier",
 				Type:     schema.TypeString,
 				Optional: true,
+				Computed: true,
 			},
 			"shipping_rate_cart_classification_value": {
 				Description: "If shipping_rate_input_type is set to CartClassification these values are used to create " +
