dafc74c0b26363b6f1f2cb401c88ea689fcd321f
Onur Gunduz <o.gunduz@labdigital.nl>
2022-04-22 18:37:13 +0200

    Remove unused import

--- commercetools/resource_project_test.go
+++ commercetools/resource_project_test.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-	"github.com/labd/commercetools-go-sdk/platform"
 	"github.com/stretchr/testify/assert"
 )
 
