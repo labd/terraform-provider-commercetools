fc47fe96628f67bafab2c41a0ce4790d971ca905
Michael van Tellingen <michael@mvantellingen.nl>
2022-05-26 15:38:48 +0100

    Update enabling debug mode in the provider
    Fixes deprecation warning

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"context"
 	"flag"
-	"log"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/labd/terraform-provider-commercetools/commercetools"
@@ -15,16 +13,8 @@ func main() {
 	flag.BoolVar(&debugMode, "debuggable", false, "set to true to run the provider with support for debuggers like delve")
 	flag.Parse()
 
-	if debugMode {
-		err := plugin.Debug(context.Background(), "registry.terraform.io/labd/commercetools",
-			&plugin.ServeOpts{
-				ProviderFunc: commercetools.Provider,
-			})
-		if err != nil {
-			log.Println(err.Error())
-		}
-	} else {
-		plugin.Serve(&plugin.ServeOpts{
-			ProviderFunc: commercetools.Provider})
-	}
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: commercetools.Provider,
+		Debug:        debugMode,
+	})
 }
