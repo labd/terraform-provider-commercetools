ada5fc0633870453ea04f71396f0e8a00f65d0e6
Thomas De Meyer <t.demeyer@labdigital.nl>
2023-08-16 16:37:01 +0200

    Added correct handling of secrets for azure_authentication in resource_api_extension.go

--- commercetools/resource_api_extension.go
+++ commercetools/resource_api_extension.go
@@ -199,7 +199,7 @@ func resourceAPIExtensionCreate(ctx context.Context, d *schema.ResourceData, m a
 	}
 
 	d.SetId(extension.ID)
-	d.Set("version", extension.Version)
+	_ = d.Set("version", extension.Version)
 
 	return resourceAPIExtensionRead(ctx, d, m)
 }
@@ -216,11 +216,11 @@ func resourceAPIExtensionRead(ctx context.Context, d *schema.ResourceData, m any
 		return diag.FromErr(err)
 	}
 
-	d.Set("version", extension.Version)
-	d.Set("key", extension.Key)
-	d.Set("destination", flattenExtensionDestination(extension.Destination, d))
-	d.Set("trigger", flattenExtensionTriggers(extension.Triggers))
-	d.Set("timeout_in_ms", extension.TimeoutInMs)
+	_ = d.Set("version", extension.Version)
+	_ = d.Set("key", extension.Key)
+	_ = d.Set("destination", flattenExtensionDestination(extension.Destination, d))
+	_ = d.Set("trigger", flattenExtensionTriggers(extension.Triggers))
+	_ = d.Set("timeout_in_ms", extension.TimeoutInMs)
 	return nil
 }
 
@@ -304,7 +304,7 @@ func expandExtensionDestination(d *schema.ResourceData) (platform.Destination, e
 	switch strings.ToLower(input["type"].(string)) {
 	case "googlecloudfunction":
 		return platform.GoogleCloudFunctionDestination{
-			Url:            input["url"].(string),
+			Url: input["url"].(string),
 		}, nil
 	case "http":
 		auth, err := expandExtensionDestinationAuthentication(input)
@@ -378,11 +378,11 @@ func flattenExtensionDestination(dst platform.Destination, d *schema.ResourceDat
 	// A destination is either GoogleCloudFunction, HTTP or AWSLambda
 	switch d := dst.(type) {
 
-    case platform.GoogleCloudFunctionDestination:
-        return []map[string]string{{
-            "type": "GoogleCloudFunction",
-            "url":  d.Url,
-        }}
+	case platform.GoogleCloudFunctionDestination:
+		return []map[string]string{{
+			"type": "GoogleCloudFunction",
+			"url":  d.Url,
+		}}
 
 	// For the HTTP Destination there are two specific authentication types:
 	// AuthorizationHeader and AzureFunctions.
@@ -413,8 +413,10 @@ func flattenExtensionDestination(dst platform.Destination, d *schema.ResourceDat
 			// so use the value from the state file instead (if it exists)
 			secretValue := ""
 			if current != nil {
-				if c, ok := current.(platform.AzureFunctionsAuthentication); ok {
-					secretValue = c.Key
+				if c, ok := current.(platform.HttpDestination); ok {
+					if auth, ok := c.Authentication.(platform.AzureFunctionsAuthentication); ok {
+						secretValue = auth.Key
+					}
 				}
 			}
 			return []map[string]string{{
