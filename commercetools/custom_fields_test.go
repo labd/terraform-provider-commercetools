ab7024f6650b3aef571fcc169dd82214d5bac53e
Bulat Khusnimardanov <bulat_khusnimardanov@epam.com>
2023-09-04 06:15:16 +0000

    Added 'set custom fields for all resources' test

--- commercetools/custom_fields_test.go
+++ commercetools/custom_fields_test.go
@@ -1,9 +1,17 @@
 package commercetools
 
 import (
+	"bytes"
+	"context"
+	"fmt"
+	"testing"
+	"text/template"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/labd/commercetools-go-sdk/platform"
 	"github.com/stretchr/testify/assert"
-	"testing"
 )
 
 var customFieldEncodeValueTests = []struct {
@@ -65,4 +73,135 @@ func TestCustomFieldEncodeValue(t *testing.T) {
 			assert.Equal(t, tt.expectedVal, encodedValue)
 		})
 	}
+}
+
+// List of the resources with custom fields support
+var customFieldResourceTypes = []string{"commercetools_channel", "commercetools_cart_discount", "commercetools_category",
+	"commercetools_customer_group", "commercetools_discount_code", "commercetools_shipping_method", "commercetools_store"}
+
+// List of the custom field types
+var customFieldTypes = []string{"Boolean", "Number", "String", "LocalizedString", "Enum", "LocalizedEnum", "Money",
+	"Date", "Time", "DateTime", "Reference", "Set"}
+
+// Test Setting of all custom fields to each supported resource
+func TestAccCustomField_Set(t *testing.T) {
+	for _, customFieldResourceType := range customFieldResourceTypes {
+		fmt.Println("Testing custom fields set for:", customFieldResourceType)
+		resourceShortName := "ct" + acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum)
+		resourceFullName := customFieldResourceType + "." + resourceShortName
+		resourceKey := "key" + resourceShortName
+		resource.Test(t, resource.TestCase{
+			PreCheck:  func() { testAccPreCheck(t) },
+			Providers: testAccProviders,
+			Steps: []resource.TestStep{
+				{
+					Config: getResourceConfig(customFieldResourceType, resourceShortName, resourceKey, customFieldTypes),
+					Check: resource.ComposeAggregateTestCheckFunc(
+						func(s *terraform.State) error {
+							customFields, err := getResourceCustomFields(s, customFieldResourceType, resourceFullName)
+							if err != nil {
+								return err
+							}
+							productType, err := testGetProductType(s, "commercetools_product_type.test")
+							if err != nil {
+								return err
+							}
+							assert.EqualValues(t, true, customFields.Fields["Boolean-field"], fmt.Sprintf("Boolean-field unexpected value for %s resource.", customFieldResourceType))
+							assert.EqualValues(t, 1234, customFields.Fields["Number-field"], fmt.Sprintf("Number-field unexpected value for %s resource.", customFieldResourceType))
+							assert.EqualValues(t, "foobar", customFields.Fields["String-field"], fmt.Sprintf("String-field unexpected value for %s resource.", customFieldResourceType))
+							assert.EqualValues(t, map[string]interface{}{"en": "Localized String", "fr": "Chaîne localisée"}, customFields.Fields["LocalizedString-field"], fmt.Sprintf("LocalizedString-field unexpected value for %s resource.", customFieldResourceType))
+							assert.EqualValues(t, "value2", customFields.Fields["Enum-field"], fmt.Sprintf("Enum-field unexpected value for %s resource.", customFieldResourceType))
+							assert.EqualValues(t, "value1", customFields.Fields["LocalizedEnum-field"], fmt.Sprintf("LocalizedEnum-field unexpected value for %s resource.", customFieldResourceType))
+							assert.EqualValues(t, map[string]interface{}{"centAmount": float64(150000), "currencyCode": "EUR", "fractionDigits": float64(2), "type": "centPrecision"}, customFields.Fields["Money-field"], fmt.Sprintf("Money-field unexpected value for %s resource.", customFieldResourceType))
+							assert.EqualValues(t, "2023-08-29", customFields.Fields["Date-field"], fmt.Sprintf("Date-field unexpected value for %s resource.", customFieldResourceType))
+							assert.EqualValues(t, "20:22:11.123", customFields.Fields["Time-field"], fmt.Sprintf("Time-field unexpected value for %s resource.", customFieldResourceType))
+							assert.EqualValues(t, "2023-08-29T20:22:11.123Z", customFields.Fields["DateTime-field"], fmt.Sprintf("DateTime-field unexpected value for %s resource.", customFieldResourceType))
+							assert.EqualValues(t, map[string]interface{}{"id": productType.ID, "typeId": "product-type"}, customFields.Fields["Reference-field"], fmt.Sprintf("Reference-field unexpected value for %s resource.", customFieldResourceType))
+							assert.EqualValues(t, []any{"ENUM-1", "ENUM-3"}, customFields.Fields["Set-field"], fmt.Sprintf("Set-field unexpected value' for %s resource.", customFieldResourceType))
+							return nil
+						},
+					),
+				},
+			},
+		})
+	}
+}
+
+func getResourceConfig(resourceType, resourceName, resourceKey string, customFields []string) string {
+	// Load templates
+	template, err := template.ParseGlob("test_templates/custom_fields_test/*")
+	if err != nil {
+		panic(err)
+	}
+
+	template_data := map[string]any{
+		"resource_type": resourceType,
+		"resource_name": resourceName,
+		"resource_key":  resourceKey,
+		"custom":        customFields,
+	}
+
+	var out bytes.Buffer
+	err = template.ExecuteTemplate(&out, "main", template_data)
+	if err != nil {
+		panic(err)
+	}
+
+	return out.String()
+}
+
+func getResourceCustomFields(s *terraform.State, resourceType, identifier string) (*platform.CustomFields, error) {
+	switch resourceType {
+	case "commercetools_channel":
+		channel, err := testGetChannel(s, identifier)
+		return channel.Custom, err
+	case "commercetools_cart_discount":
+		cartDiscount, err := testGetCartDiscount(s, identifier)
+		return cartDiscount.Custom, err
+	case "commercetools_category":
+		category, err := testGetCategory(s, identifier)
+		return category.Custom, err
+	case "commercetools_customer_group":
+		customerGroup, err := testGetCustomerGroup(s, identifier)
+		return customerGroup.Custom, err
+	case "commercetools_discount_code":
+		discountCode, err := testGetDiscountCode(s, identifier)
+		return discountCode.Custom, err
+	case "commercetools_shipping_method":
+		shippingMethod, err := testGetShippingMethod(s, identifier)
+		return shippingMethod.Custom, err
+	case "commercetools_store":
+		store, err := testGetStore(s, identifier)
+		return store.Custom, err
+	default:
+		panic(fmt.Sprintf("Unknown resource type %s", resourceType))
+	}
+}
+
+func testGetCategory(s *terraform.State, identifier string) (*platform.Category, error) {
+	rs, ok := s.RootModule().Resources[identifier]
+	if !ok {
+		return nil, fmt.Errorf("Category %s not found", identifier)
+	}
+
+	client := getClient(testAccProvider.Meta())
+	result, err := client.Categories().WithId(rs.Primary.ID).Get().Execute(context.Background())
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func testGetShippingMethod(s *terraform.State, identifier string) (*platform.ShippingMethod, error) {
+	rs, ok := s.RootModule().Resources[identifier]
+	if !ok {
+		return nil, fmt.Errorf("Shipping Method %s not found", identifier)
+	}
+
+	client := getClient(testAccProvider.Meta())
+	result, err := client.ShippingMethods().WithId(rs.Primary.ID).Get().Execute(context.Background())
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
 }
