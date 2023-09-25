45a0bc5a6a32ef9356629f74406fe5dce2f35e73
Thomas De Meyer <t.demeyer@labdigital.nl>
2023-09-25 10:03:29 +0200

    reactivated tests

--- internal/resources/project/model_test.go
+++ internal/resources/project/model_test.go
@@ -1,6 +1,9 @@
 package project
 
 import (
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/labd/terraform-provider-commercetools/internal/customtypes"
+	"github.com/labd/terraform-provider-commercetools/internal/utils"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-framework/types"
@@ -64,94 +67,94 @@ func TestUpdateActions(t *testing.T) {
 		plan   Project
 		action platform.ProjectUpdate
 	}{
-		//{
-		//	name: "Default",
-		//	state: Project{
-		//		Version:               types.Int64Value(1),
-		//		ID:                    types.StringValue("my-project"),
-		//		Key:                   types.StringValue("my-project"),
-		//		Name:                  types.StringValue("my project"),
-		//		Countries:             []types.String{types.StringValue("US")},
-		//		ShippingRateInputType: types.StringValue("CartValue"),
-		//	},
-		//	plan: Project{
-		//		Version: types.Int64Value(1),
-		//		ID:      types.StringValue("my-project"),
-		//		Key:     types.StringValue("my-project"),
-		//		Name:    types.StringValue("my new name"),
-		//		Countries: []types.String{
-		//			types.StringValue("NL"),
-		//			types.StringValue("DE"),
-		//		},
-		//		ShippingRateInputType: types.StringValue("CartClassification"),
-		//		ShippingRateCartClassificationValue: []models.CustomFieldLocalizedEnumValue{
-		//			{
-		//				Key: types.StringValue("Light"),
-		//				Label: customtypes.NewLocalizedStringValue(map[string]attr.Value{
-		//					"nl": types.StringValue("licht"),
-		//					"en": types.StringValue("light"),
-		//				}),
-		//			},
-		//		},
-		//	},
-		//	action: platform.ProjectUpdate{
-		//		Version: 1,
-		//		Actions: []platform.ProjectUpdateAction{
-		//			platform.ProjectChangeCountriesAction{
-		//				Countries: []string{"NL", "DE"},
-		//			},
-		//			platform.ProjectChangeNameAction{
-		//				Name: "my new name",
-		//			},
-		//			platform.ProjectSetShippingRateInputTypeAction{
-		//				ShippingRateInputType: platform.CartClassificationType{
-		//					Values: []platform.CustomFieldLocalizedEnumValue{
-		//						{
-		//							Key: "Light",
-		//							Label: platform.LocalizedString{
-		//								"en": "light",
-		//								"nl": "licht",
-		//							},
-		//						},
-		//					},
-		//				},
-		//			},
-		//		},
-		//	},
-		//},
-		//{
-		//	name: "Carts Configuration",
-		//	state: Project{
-		//		Version: types.Int64Value(1),
-		//		Carts: []Carts{
-		//			{
-		//				CountryTaxRateFallbackEnabled:   types.BoolValue(true),
-		//				DeleteDaysAfterLastModification: types.Int64Value(10),
-		//			},
-		//		},
-		//	},
-		//	plan: Project{
-		//		Version: types.Int64Value(1),
-		//		Carts: []Carts{
-		//			{
-		//				CountryTaxRateFallbackEnabled:   types.BoolValue(false),
-		//				DeleteDaysAfterLastModification: types.Int64Value(90),
-		//			},
-		//		},
-		//	},
-		//	action: platform.ProjectUpdate{
-		//		Version: 1,
-		//		Actions: []platform.ProjectUpdateAction{
-		//			platform.ProjectChangeCartsConfigurationAction{
-		//				CartsConfiguration: platform.CartsConfiguration{
-		//					CountryTaxRateFallbackEnabled:   utils.BoolRef(false),
-		//					DeleteDaysAfterLastModification: utils.IntRef(90),
-		//				},
-		//			},
-		//			platform.ProjectChangeCountryTaxRateFallbackEnabledAction{CountryTaxRateFallbackEnabled: false},
-		//		},
-		//	},
-		//},
+		{
+			name: "Default",
+			state: Project{
+				Version:               types.Int64Value(1),
+				ID:                    types.StringValue("my-project"),
+				Key:                   types.StringValue("my-project"),
+				Name:                  types.StringValue("my project"),
+				Countries:             []types.String{types.StringValue("US")},
+				ShippingRateInputType: types.StringValue("CartValue"),
+			},
+			plan: Project{
+				Version: types.Int64Value(1),
+				ID:      types.StringValue("my-project"),
+				Key:     types.StringValue("my-project"),
+				Name:    types.StringValue("my new name"),
+				Countries: []types.String{
+					types.StringValue("NL"),
+					types.StringValue("DE"),
+				},
+				ShippingRateInputType: types.StringValue("CartClassification"),
+				ShippingRateCartClassificationValue: []models.CustomFieldLocalizedEnumValue{
+					{
+						Key: types.StringValue("Light"),
+						Label: customtypes.NewLocalizedStringValue(map[string]attr.Value{
+							"nl": types.StringValue("licht"),
+							"en": types.StringValue("light"),
+						}),
+					},
+				},
+			},
+			action: platform.ProjectUpdate{
+				Version: 1,
+				Actions: []platform.ProjectUpdateAction{
+					platform.ProjectChangeCountriesAction{
+						Countries: []string{"NL", "DE"},
+					},
+					platform.ProjectChangeNameAction{
+						Name: "my new name",
+					},
+					platform.ProjectSetShippingRateInputTypeAction{
+						ShippingRateInputType: platform.CartClassificationType{
+							Values: []platform.CustomFieldLocalizedEnumValue{
+								{
+									Key: "Light",
+									Label: platform.LocalizedString{
+										"en": "light",
+										"nl": "licht",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "Carts Configuration",
+			state: Project{
+				Version: types.Int64Value(1),
+				Carts: []Carts{
+					{
+						CountryTaxRateFallbackEnabled:   types.BoolValue(true),
+						DeleteDaysAfterLastModification: types.Int64Value(10),
+					},
+				},
+			},
+			plan: Project{
+				Version: types.Int64Value(1),
+				Carts: []Carts{
+					{
+						CountryTaxRateFallbackEnabled:   types.BoolValue(false),
+						DeleteDaysAfterLastModification: types.Int64Value(90),
+					},
+				},
+			},
+			action: platform.ProjectUpdate{
+				Version: 1,
+				Actions: []platform.ProjectUpdateAction{
+					platform.ProjectChangeCartsConfigurationAction{
+						CartsConfiguration: platform.CartsConfiguration{
+							CountryTaxRateFallbackEnabled:   utils.BoolRef(false),
+							DeleteDaysAfterLastModification: utils.IntRef(90),
+						},
+					},
+					platform.ProjectChangeCountryTaxRateFallbackEnabledAction{CountryTaxRateFallbackEnabled: false},
+				},
+			},
+		},
 		{
 			name: "Create with bool unknown",
 			state: Project{
